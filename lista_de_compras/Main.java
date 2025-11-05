a9b045fe610fd6a70b32de63273a02d414068c5f
Arthur Gabriel <arthurggbbccss@gmail.com>
2025-11-05 20:38:44 -0300

    Update Main.java

--- lista_de_compras/Main.java
+++ lista_de_compras/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ListaDeCompras{
+public class Main{
 
     public static void main(String[] args) {
         
@@ -66,3 +66,4 @@ public class ListaDeCompras{
         }
     }
 }
+
